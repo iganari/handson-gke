5e6230c38de86c861e228ec554fd7d5901bf51dc
iganari <cigaguri@gmail.com>
2020-09-05 16:53:00 +0900

    update python image

--- 10_hello-world/python/main.py
+++ 10_hello-world/python/main.py
@@ -1,11 +1,18 @@
 # https://github.com/JasonHaley/hello-python/tree/master/app
 
 from flask import Flask
+import socket
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello from Python!"
+
+@app.route('/<mypath>')
+def show_path(mypath):
+    host_name = socket.gethostname()
+    host_ip   = socket.gethostbyname(host_name)
+    return 'Your Path is "/{}" .\nFrom Hands On GKE.\nHostName: {}\nIP: {}'.format(mypath, host_name, host_ip)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
