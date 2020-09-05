2e59e7249ffb4e0105b2f3910c155416b4ed704f
iganari <cigaguri@gmail.com>
2020-09-05 18:51:05 +0900

    add current time

--- 10_hello-world/python/main.py
+++ 10_hello-world/python/main.py
@@ -2,6 +2,8 @@
 
 from flask import Flask
 import socket
+import datetime
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -10,9 +12,10 @@ def hello():
 
 @app.route('/<mypath>')
 def show_path(mypath):
-    host_name = socket.gethostname()
-    host_ip   = socket.gethostbyname(host_name)
-    return 'Your Path is "/{}" .\nFrom Hands On GKE.\nHostName: {}\nIP: {}'.format(mypath, host_name, host_ip)
+    host_name    = socket.gethostname()
+    host_ip      = socket.gethostbyname(host_name)
+    current_time = datetime.datetime.now()
+    return 'Your Path is "/{}" .\nFrom Hands On GKE.\nHostName: {}\nIP: {}\nCurrent time: {}\n'.format(mypath, host_name, host_ip, current_time)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
